82b7e61f0f3c58dc4f4386a40d793f69d4b2cfe6
Binh Tran <binhtran@Sogetis-MacBook-Pro-2.local>
2019-01-14 17:27:06 -0600

    changes

--- src/assets/scripts/typewriter-animation.js
+++ src/assets/scripts/typewriter-animation.js
@@ -82,7 +82,7 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
             return TextScramble;
         }();
 
-        var phrases = ['Consultant', 'Software developer', 'Web designer', 'Mobile App dev'];
+        var phrases = ['Consultant', 'Programmer, 'Web developer', 'Mobile App dev', "Learner", "Volunteer", "Freelance"];
 
         var el = document.querySelector('.text');
         var fx = new TextScramble(el);
